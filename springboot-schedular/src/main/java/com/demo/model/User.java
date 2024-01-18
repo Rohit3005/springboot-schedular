1b8091b90a9ef0a485faa98c52d6a818737d003d
bdo <bdo>
2024-01-18 15:43:59 +0530

    initial commit

---
+++ springboot-schedular/src/main/java/com/demo/model/User.java
@@ -0,0 +1,23 @@
+package com.demo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int userId;
+
+    private String userName;
+}
